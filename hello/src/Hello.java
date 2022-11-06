4ba6c3c8f365b6bf84cdd8a0730d3df9a6f00e38
YoonHwikyung <yoonhwikyung@naver.com>
2022-11-06 15:44:39 +0900

    test : 주석 깨짐 확인을 위한 테스트

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 
-	public static void main(String[] args) { // ���� 
+	public static void main(String[] args) { //주석 한글 꺠짐 확인 필요
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 	}
