24016dac44909fea48072685d64e099296d8f2ad
YoonHwikyung <yoonhwikyung@naver.com>
2022-11-08 23:54:44 +0900

    첫 번째 Java프로그램 만들기

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -1,8 +1,8 @@
 
 public class Hello {
 
-	public static void main(String[] args) { //주석 한글 꺠짐 확인 필요
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
 		System.out.println("hello");
 	}
 
