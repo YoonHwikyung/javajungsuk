1f5d0993ca9db9dc62a10a5c473baca1a66e4381
YoonHwikyung <yoonhwikyung@naver.com>
2022-11-08 23:59:39 +0900

    화면에 글자 출력하기, 덧셈 뺄셈 계산하기

---
+++ ch02/src/Ex2_1.java
@@ -0,0 +1,19 @@
+// 화면에 글자 출력하기, 덧셈 뺄셈 계산하기
+public class Ex2_1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// syso ctrl+space 자동완성
+		// ctrl+alt+shift+down 행단위 복사
+		// alt+shift+a 컬럼 편집 모드(토글)
+		// print() - 출력 후 줄바꿈 X
+		// println() - 출력 후 줄바꿈 O
+		System.out.println(5+3); // 덧셈
+		System.out.println(5-3); // 뺄셈
+		System.out.println(5*3); // 곱셈
+		System.out.println(5/3); // 나눗셈
+		
+	}
+	
+}
