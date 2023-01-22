695f7cb541328684eabf11b2817bf5ef0a9ec075
YoonHwikyung <yoonhwikyung@naver.com>
2023-01-22 22:55:05 +0900

    ch2_15,16,17 정수형의 오버플로우, 타입간의 변환방법

---
+++ ch02/src/Ex2_12.java
@@ -0,0 +1,16 @@
+
+public class Ex2_12 {
+
+	public static void main(String[] args) {
+		String str = "3";
+		
+		// "3" => '3'
+		System.out.println(str.charAt(0) - '0'); // '3' - '0' = 3
+		System.out.println('3' - '0' + 1); // 4
+		System.out.println(Integer.parseInt("3") + 1); // 4
+		System.out.println("3" + 1); // "31"
+		System.out.println(3 + '0'); // 51 ('0'은 숫자로 48)
+		System.out.println((char)(3 + '0')); // '3'
+	}
+
+}
